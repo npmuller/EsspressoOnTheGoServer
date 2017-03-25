0d909b39fa14899d7927e5aa3b29ae737e9a45bd
Nate <npmuller67@gmail.com>
2017-03-25 14:43:34 +0000

    Added DB config module.

---
+++ database/database.js
@@ -0,0 +1,19 @@
+// Set up connection
+var env = process.env.NODE_ENV || "development";
+var config = require('../config/config.json')[env];
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : config.username,
+    password : config.password,
+    database : config.database,
+    charset  : 'utf8'
+  }
+});
+
+// Instantiate Bookshelf using connection
+var bookshelf = require('bookshelf')(knex);
+
+// Export connection
+module.exports = bookshelf;
