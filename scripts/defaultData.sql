56c1378dc4e74e1f3aeb01e5aeb95bd0e61c0940
Nate <npmuller67@gmail.com>
2017-03-27 20:42:32 +0000

    default database values.

---
+++ scripts/defaultData.sql
@@ -0,0 +1,8 @@
+-- coffee types
+insert into coffee_type values (1, 'Dark Roast', now()),(2, 'Medium', now()),(3, 'Blonde', now());
+
+-- device types
+ insert into device_type values (1, 'Espresso Machine', now()), (2, 'Android Device', now());
+ 
+ -- software versions
+ insert into software values (1, '0.0.1', '/software/android/0.0.1', now());
