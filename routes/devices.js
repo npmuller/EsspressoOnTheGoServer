c1c1e601222450b7188ecb1e4e1dc866dca281ac
Nate <npmuller67@gmail.com>
2017-03-27 20:46:42 +0000

    Finished device registration funciton.

--- routes/devices.js
+++ routes/devices.js
@@ -7,23 +7,28 @@ var models = require('../models/models.js');
 // Get every user associated with a device
 router.post('/registerDevice', bodyParser.urlencoded({extended: true}), function (req, res) {
     var deviceIdentifier = req.body.deviceIdentifier;
+    var wifiMAC = req.body.macAddress;
+    // TODO : get real device type
     console.debug("Registering device " + deviceIdentifier + ".");
     models.device.forge({
-     device_identifier: deviceIdentifier
+     device_identifier: deviceIdentifier,
+     wifi_mac_address: wifiMAC,
+     device_type_id: 2
    })
     .save()
-    .then(function (user) {
-      if (!user) {
-        console.log("User " + un + " does not exist, or some other error has occured.");
+    .then(function (device) {
+      if (!device) {
+        // TODO : better logging
+        console.error("Error registering device.");
         // user not found
-        res.status(500).json({error: true, data: {message: "User not found or internal server error."}});
+        res.status(500).json({error: true, data: {message: "Error registering device."}});
       }
       else {
         // success! return:
         //     json object containing session id, user id, and device id if
         //     applicable.
-        console.log("User " + un + " successfully signed in!");
-        res.json({message: "signed in!"})
+        console.log("Device " + device.device_identifier + " successfully registered!");
+        res.json({deviceId: device.id})
       }
   })
   .catch(function (err) {
