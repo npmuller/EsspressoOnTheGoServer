7006bfc84363d5774810c0ff866e9f3817c7a85e
Nate <npmuller67@gmail.com>
2017-03-27 21:39:24 +0000

    Added all default data nevessary for testing.

--- scripts/defaultData.sql
+++ scripts/defaultData.sql
@@ -6,3 +6,6 @@ insert into coffee_type values (1, 'Dark Roast', now()),(2, 'Medium', now()),(3,
  
  -- software versions
  insert into software values (1, '0.0.1', '/software/android/0.0.1', now());
+ 
+ -- brew setting types
+ insert into brew_setting_type values (1,'Temperature',2,now(),now()),(2,'Pressure',2,now(),now()),(3,'Brew Time',1,now(),now());
