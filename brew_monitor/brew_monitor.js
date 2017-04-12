6dcc3c2b2157b0f8dbb5402a967bd81b8fbc9033
Nathan Muller <npmuller67@gmail.com>
2017-04-12 22:59:56 +0000

    Test brew monitor

--- brew_monitor/brew_monitor.js
+++ brew_monitor/brew_monitor.js
@@ -1,1 +1,23 @@
-// BULLSHIT CONTENT
+var _ = require('lodash');
+var models = require('../models/models.js')
+var collections = require('../models/collections.js')
+
+var ish = true;
+while(ish) {
+    console.info('Running brew monitor.');
+    // Get the brew schedule for all devices
+    var scheduleSetting = models.device_setting.forge({
+         brew_setting_type_id: 3
+    });
+    collections.DeviceBrewSettings.forge([scheduleSetting])
+    .fetch()
+    .then(function(settings) {
+        _.each(settings, function(setting) {
+            console.info(setting);
+        });
+    }).catch(function(ex) {
+        console.error("Error getting brew schedules for scheduling.");
+    });
+    ish = false;
+}
+
